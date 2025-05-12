c9e945136ed8d5a0da2e2ce43cb85b800292bd9d
olober76 <dutakupri@gmail.com>
2025-05-13 01:24:57 +0700

    define the model

---
+++ app/config/model.py
@@ -0,0 +1,17 @@
+
+## CHOOSE THE MODEL HERE
+class WHISPER_MODELS:
+    # ENGLISH BASED
+    tiny_en = 'tiny.en'
+    base_en = 'base.en'
+    small_en = 'small.en'
+    medium_en = 'medium.en'
+
+    # MULTILINGUAL BASED
+    tiny = 'tiny'
+    base = 'base'
+    small = 'small'
+    medium = 'medium'
+    large = 'large'
+    turbo = 'turbo'
+    
