325b6cb8d4a58c48da9bb6d3797b59ae9ad2b9c4
olober76 <dutakupri@gmail.com>
2025-05-14 00:44:34 +0700

    refactoring  some code

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import tkinter as tk
 from app.utils.gui_Service import AIAssistantApp
-from app.service.ollama_process import handle_transcription
-
+from app.service.llama_process import handle_transcription
+from app.service.core import process_llama_result
 
 
 if __name__ == "__main__":
     print("LOG : AI Assistant - Starting...")
     root = tk.Tk()
     app = AIAssistantApp(root)
-    ollama_result = app.set_transcription_callback(handle_transcription)
+    
+    # Set up the callback dengan wrapper function
+    app.set_transcription_callback(lambda x: process_llama_result(handle_transcription(x)))
+    
     root.mainloop()
